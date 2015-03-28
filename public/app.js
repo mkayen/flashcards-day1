faf35d9500c4aa0296b5bb3014fca212e99d3781
Max Kayen <mkayen@gmail.com>
2015-03-28 15:06:57 -0400

    Flashcards work with multiple questions

--- public/app.js
+++ public/app.js
@@ -3,14 +3,14 @@ var app = angular.module('FlashCards', []);
 var scope; //For Debugging
 
 app.controller('FlashCardController', function($scope){
-	$scope.flashCard = {
-	    question: 'What is Angular?',
-	    answers: [
-	        { text: 'A front-end framework for great power!', correct: true },
-	        { text: 'Something lame, who cares, whatever.', correct: false },
-	        { text: 'Some kind of fish, right?', correct: false }
-	    ]
-	};
+	// $scope.flashCard = {
+	//     question: 'What is Angular?',
+	//     answers: [
+	//         { text: 'A front-end framework for great power!', correct: true },
+	//         { text: 'Something lame, who cares, whatever.', correct: false },
+	//         { text: 'Some kind of fish, right?', correct: false }
+	//     ]
+	// };
   
 	$scope.answerQuestion = function (answer) {
 				
